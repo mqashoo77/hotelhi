d0120e4f0912368c807aad1196a573d959bf516c
Mohammed Buirat <1192896@student.birzeit.edu>
2023-07-06 19:28:50 +0300

    Hotel Activity

--- app/src/main/java/com/example/hotelhi/activites/HotelDetailsActivity.java
+++ app/src/main/java/com/example/hotelhi/activites/HotelDetailsActivity.java
@@ -2,15 +2,61 @@ package com.example.hotelhi.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.hotelhi.R;
+import com.example.hotelhi.entity.Hotel;
+
+
 
 public class HotelDetailsActivity extends AppCompatActivity {
+    ImageView backHotel;
+    TextView hotelName1;
+    TextView hotelName2;
+    TextView hotelCityText;
+    TextView hotelRatingText;
+    TextView hotelNumRatingText;
+    TextView hotelPriceText;
+    Button hotelChooseRoomBtn;
+
+    Hotel hotel;
+
+    void setItems(){
+        backHotel=findViewById(R.id.hotelBackBtn);
+        hotelName1=findViewById(R.id.hotelNameText1);
+        hotelName2=findViewById(R.id.hotelNameText2);
+        hotelCityText=findViewById(R.id.hotelCityText);
+        hotelRatingText=findViewById(R.id.hotelRatingText);
+        hotelNumRatingText=findViewById(R.id.hotelNumOfRatingsText);
+        hotelPriceText=findViewById(R.id.priceHotelText);
+        hotelChooseRoomBtn=findViewById(R.id.hotelRoomBtn);
+        hotelName2.setText(hotel.getName());
+        hotelName1.setText(hotel.getName());
+        hotelCityText.setText(hotel.getCity());
+        hotelRatingText.setText(Double.toString(hotel.getRatings()));
+        hotelNumRatingText.setText(Integer.toString(hotel.getNumOfRatings()));
+
+        hotelChooseRoomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HotelDetailsActivity.this,RoomBookingActivity.class);
+            }
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel_details);
+        hotel = new Hotel(1, "Example Hotel", "123 Example Street", "Example City", "Example Country", "123-456-7890", "info@example.com", "example_image.jpg", 5, 100, 4.5);
+        setItems();
+
     }
 }
